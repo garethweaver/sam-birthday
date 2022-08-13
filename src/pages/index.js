d6f2c7dd2584074a4b4e918d03874d5ad675f627
Gareth Weaver <reg@garethweaver.com>
2022-08-13 17:21:52 +0100

    feature(tweaks)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,16 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Hey, guess what?</h1>
-    <p>In case you didn't know Sam just turned 30, how much do you really know about her though?</p>
-    <Link
-      className="Blockbutton"
-      to="/questions/1">
-      Let's go!
-    </Link>
+  <Layout className="Index">
+    <div>
+      <h1>Hey, guess what?</h1>
+      <p>In case you didn't know Sam just turned 30, how much do you really know about her though?</p>
+      <Link
+        className="Blockbutton"
+        to="/questions/1">
+        Let's go!
+      </Link>
+    </div>
   </Layout>
 )
 
